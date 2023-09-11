a359fcfc8c532eb658aba1aaa7439b01edce9a69
neev0511 <allofbakwas@gmail.com>
2023-09-11 19:06:50 +0530

    Gideon Virtual Assistant

---
+++ main.py
@@ -0,0 +1,200 @@
+import pyttsx3
+import speech_recognition as sr
+import pyjokes
+import webbrowser
+import spotipy
+import requests
+from bs4 import BeautifulSoup
+import feedparser
+import json
+import wolframalpha
+
+
+# FUCNTIONS
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def listen():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio_text = r.listen(source)
+
+    try:
+        return r.recognize_google(audio_text)
+    except:
+        speak("Voice Recognition Failed")
+
+
+def in_task(list1,task):
+    for i in list1:
+        if i in task:
+            return True
+
+
+def tell_jokes():
+    for _ in range(5):
+        speak(pyjokes.get_joke())
+
+
+def google(task):
+    webbrowser.open("https://www.google.com/search?q="+task)
+
+
+def play_song():
+    username = '3165do7y2lvhed6ax7o3guhrhq7m'
+    clientID = '5489c916e67e4871bd191ae179caa050'
+    clientSecret = 'ee3a0548119d44c2b03d320d2306ee4a'
+    redirect_uri = 'http://google.com/callback/'
+    oauth_object = spotipy.SpotifyOAuth(clientID, clientSecret, redirect_uri)
+    token_dict = oauth_object.get_access_token()
+    token = token_dict['access_token']
+    spotifyObject = spotipy.Spotify(auth=token)
+    user_name = spotifyObject.current_user()
+    speak("Which song would you like to listen to?")
+    song = listen()
+    results = spotifyObject.search(song, 1, 0, "track")
+    songs_dict = results['tracks']
+    song_items = songs_dict['items']
+    song = song_items[0]['external_urls']['spotify']
+    webbrowser.open(song)
+
+
+def tell_temperature():
+    send_url = 'http://ipinfo.io/json'
+    r = requests.get(send_url)
+
+    j = json.loads(r.text)
+
+    city = j['city']
+
+    # creating url and requests instance
+    url = "https://www.google.com/search?q="+"weather"+city
+    html = requests.get(url).content
+
+    # getting raw data
+    soup = BeautifulSoup(html, 'html.parser')
+    temp = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
+
+    # speaking all data
+    speak("Temperature is " + temp)
+
+
+def news(feed_url_tech):
+    # Get the feed data
+    feed_tech = feedparser.parse(feed_url_tech)
+
+    # Extract the titles and links of the top 10 entries
+    entries_tech = feed_tech['entries'][:5]
+    titles_tech = [entry['title'] for entry in entries_tech]
+
+    for i in titles_tech:
+        speak(i)
+
+
+def wolfram(task):
+    res = client.query(task)
+    pods = list(res.pods)
+
+    if len(pods) > 0:
+        result = next(res.results).text
+        speak(result)
+    else:
+        return False
+
+
+def movie_rec():
+    speak("Which genre would you prefer")
+    genre = listen()
+    if in_task(["science","sci-fi","fiction"],genre):
+        genre_id = 878
+    elif in_task(["comedy"],genre):
+        genre_id = 80
+    elif in_task(["action"],genre):
+        genre_id = 28
+
+    api_key = "e58ea43ba5662ba3e122d41616822faf"   
+    try: 
+        url = f"https://api.themoviedb.org/3/discover/movie?api_key={api_key}&with_genres={genre_id}"
+    except:
+        speak("Error occured")
+
+    response = requests.get(url)
+    data = response.json()
+
+    recs = data["results"][:5]
+
+    if len(recs) > 0:
+        speak(f"Here are 5 movie recommendations")
+
+        for r in recs:
+            speak(r["title"]+ " " + str(r["vote_average"])+ " "+ "out of 10")
+
+    else:
+        speak("No recommendations found")
+
+
+
+# MAIN CODE
+
+# General
+NAME = "Neev"
+FULL_NAME = "Neev Jain"
+EMAIL = "neev0511jain@gmail.com"
+
+# Speaking
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[2].id) 
+
+# Listen
+r = sr.Recognizer()
+
+# Wolframalpha
+APP_ID = "XPTYKU-VGAGQQUTH8" 
+client = wolframalpha.Client(APP_ID)
+
+# MAIN
+
+speak("Hello Mister Neev. How may I help you?")
+flag = False
+while True:
+    if flag:
+        speak("Would you like my assistance with anything else?")
+
+    task = listen()
+    if in_task(["joke","jokes"],task):
+        tell_jokes()
+
+    elif in_task(["song","songs"],task):
+        play_song()
+
+    elif in_task(["temperature","weather"],task):
+        tell_temperature()
+
+    elif in_task(["news","newspaper","newsletter"],task):
+        speak("Which news would you like")
+
+        query = listen()
+
+        if in_task(["general","normal","indian"],query):
+            news("https://timesofindia.indiatimes.com/rssfeedstopstories.cms")
+        elif in_task(["tech","technological","geek","nerd"],query):
+            news("https://timesofindia.indiatimes.com/rssfeeds/66949542.cms")
+    
+    elif in_task(["movie"],task):
+        movie_rec()
+
+    elif in_task(["thank"],task):
+        speak("You're welcome")
+        break
+
+    elif in_task(["exit","break","quit","stop","nothing"],task):
+        break
+
+    else:
+        x = wolfram(task)
+        if x == False:
+            google(task)
+    flag = True
